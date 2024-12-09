1c67335a876dad91a1b14a4db5e98dbfda5cf63d
Dobry Nikolov <dobry989@gmail.com>
2024-12-09 11:17:59 +0200

    Simple gRPC client and server

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "context"
+  "log"
+  "time"
+  
+  pb "github.com/bovf/gRPCLearning/proto"
+  "google.golang.org/grpc"
+)
+
+func main() {
+  conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+  if err != nil {
+    log.Fatalf("did not connect: %v", err)
+  }
+  defer conn.Close()
+  c := pb.NewGreeterClient(conn)
+
+  ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+  defer cancel()
+  r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "gRPC"})
+  if err != nil {
+    log.Fatalf("could not greet: %v", err)
+  }
+  log.Printf("Greetings: %s", r.GetMessage())
+}
